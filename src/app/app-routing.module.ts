a088bd948c5be5d78fcfa43de84a926b3179fc61
Ganesh Marmat <ganeshmarmat@gamil.com>
2022-07-16 23:48:18 +0530

    changes with one sync

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomAuthGuard } from './custom-auth.guard';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { CSIModule } from './feature-modules/csi/csi.module';
+import { CustomerModule } from './feature-modules/customer/customer.module';
+import { QuotationComponent } from './feature-modules/customer/quotation/quotation.component';
+import { DesignModule } from './feature-modules/design/design.module';
+import { VendorManagementModule } from './feature-modules/vendor-management/vendor-management.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {path:'',redirectTo:"/dashboard" ,pathMatch: 'full',},
+  {
+    path:'dashboard',//redirectTo:'/dashboard', pathMatch: 'full',
+    component: DashboardComponent,  canActivate: [CustomAuthGuard],
+    children:[
+      {
+        path:'customer',loadChildren:()=>CustomerModule
+      },
+      {
+        path:'csi',loadChildren:()=>CSIModule
+      },
+      {
+        path:'design',loadChildren:()=>DesignModule
+      },
+      {
+        path: 'vendor', loadChildren:()=>VendorManagementModule
+      }
+    ]
+  
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TokenStorageService } from './erp-services/token-storage/token-storage.service';
+import { authInterceptorProviders } from './common/auth/auth.interceptor';
+import { NavigationControlComponent } from './common/controls/navigation-control/navigation-control/navigation-control.component';
+
+import { MaterialExampleModule } from './common/ngmaterial/materialex.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavigationControlComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    MaterialExampleModule,
+    MatNativeDateModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [TokenStorageService,authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
